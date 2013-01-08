48450d12be6397a826e5539e7bc336078ecbba89
Trang Ho <carepjan@gmail.com>
2013-01-08 16:13:09 +0700

    Initiate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onoff::Application.config.secret_token = 'fd0f3f1f1977264dba875a18f0cfb77603c497c2ab32700d6978914a85645cf9a7fd4fd10b6ba4b8a21b6fd2da79bcfd3801ceca1269222f7aa233c04ff75215'
