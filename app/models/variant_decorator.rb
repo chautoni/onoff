248562f0d861863db6d2311f21d27bd3351cc5b3
Trang Ho <trang.ho@eastagile.com>
2013-03-01 13:02:33 +0700

    Update Vi translation + add common.css to precompile list in production

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Variant.class_eval do
   accepts_nested_attributes_for :images, :allow_destroy => true
 	accepts_nested_attributes_for :option_values, :allow_destroy => true
-	attr_accessible :images, :images_attributes, :count_on_hand
+	attr_accessible :images, :images_attributes, :count_on_hand, :is_master
 end
