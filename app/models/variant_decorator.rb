d5268b3025d35cf5894098fb3beb77728e1a460f
Trang Ho <carepjan@gmail.com>
2013-03-19 23:46:04 +0700

    - Delete duplicated variants of a product - Change display of Option value to use name instead of presentation (for now presentation is used as sky)

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -3,6 +3,7 @@ Spree::Variant.class_eval do
   accepts_nested_attributes_for :option_values, :allow_destroy => true
   attr_accessible :images, :images_attributes, :count_on_hand, :is_master
   validate :sku, :presence => true
+  after_save :check_duplicate_variants
 
   before_save :assign_sku
 
@@ -10,8 +11,30 @@ Spree::Variant.class_eval do
 	self.option_values.detect { |o| o.option_type.name == opt_name }.try(:name)
   end
 
+  def options_text
+    values = self.option_values.joins(:option_type).order("#{Spree::OptionType.table_name}.position asc")
+
+    values.map! do |ov|
+      "#{::I18n.t ov.option_type.name}: #{ov.name}"
+    end
+
+    values.to_sentence({ :words_connector => ", ", :two_words_connector => ", " })
+  end
+
   private
   def assign_sku
   	self.sku = "#{self.product.product_sku}#{self.option_value('color')}#{self.option_value('size')}"
+  end
+
+  def check_duplicate_variants
+    variants_options = self.product.variants.map { |v| { :id => v.id, :option_values => { :color => v.option_value('color'), :size => v.option_value('size') } } }
+    duplicated_variants = []
+    variants_options.each do |v|
+      if (variants_options - [v]).map{ |e| e[:option_values] }.include?(v[:option_values])
+        id = variants_options.select{ |e| e[:option_values] == v[:option_values] }.map{ |e| e[:id] }.sort.last
+        duplicated_variants << Spree::Variant.find(id)
+      end
+    end
+    duplicated_variants.uniq.map(&:destroy)
   end
 end
