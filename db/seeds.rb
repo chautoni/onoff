1ef6095d305fdb3de7c4b2ed5426fc884ac929d9
Thach Chau <rog.kane@gmail.com>
2013-03-20 21:19:16 +0700

    Edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 
 
 
- # Spree::Core::Engine.load_seed if defined?(Spree::Core)
- # Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+# Spree::Core::Engine.load_seed if defined?(Spree::Core)
+# Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 # Loads seed data out of default dir
 default_path = File.join(File.dirname(__FILE__), 'default')
@@ -23,19 +23,15 @@ Spree::OptionType.create(:name => 'size', :presentation => 'Size') unless Spree:
 
 Spree::Property.create(:name => 'unit', :presentation => 'Đơn Vị Tính') unless Spree::Property.find_by_name('unit')
 
-Spree::Zone.create(:name => 'Asia') unless Spree::Zone.find_by_name('Asia')
+Spree::Zone.find_or_create_by_name('Asia')
 asia_zone = Spree::Zone.find_by_name('Asia')
 asia_zone.zone_members.create(:zoneable_id => 219, :zoneable_type => 'Spree::Country') if asia_zone.zone_members.empty?
 
-Spree::Role.create(:name => 'super_admin') unless Spree::Role.find_by_name('super_admin')
+Spree::Role.find_or_create_by_name('super_admin')i
 
-Spree::Taxonomy.create(:name => 'Bộ sưu tập') unless Spree::Taxonomy.find_by_name('Bộ sưu tập')
-collection = Spree::Taxonomy.find_by_name('Bộ sưu tập')
-sport = Spree::Taxon.find_by_name('Thể thao')
-collection.taxons.create(:name => 'Thể thao') unless collection.taxons.include?(sport)
-energetic = Spree::Taxon.find_by_name('Năng động')
-collection.taxons.create(:name => 'Năng động') unless collection.taxons.include?(energetic)
-office = Spree::Taxon.find_by_name('Công sở')
-collection.taxons.create(:name => 'Công sở') unless collection.taxons.include?(office)
-golf = Spree::Taxon.find_by_name('Golf')
-collection.taxons.create(:name => 'Golf') unless collection.taxons.include?(golf)
+Spree::Taxonomy.find_or_create_by_name('Dòng sản phẩm')
+collection = Spree::Taxonomy.find_or_create_by_name('Bộ sưu tập')
+collection.taxons.find_or_create_by_name('Thể thao')
+collection.taxons.find_or_create_by_name('Năng động')
+collection.taxons.find_or_create_by_name('Công sở')
+collection.taxons.find_or_create_by_name(:name => 'Golf')
