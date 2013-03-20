b9261401221873967ed75e2583f10597fac9f8fc
Trang Ho <carepjan@gmail.com>
2013-03-20 23:19:47 +0700

    Minor UI fix for search filter

--- app/assets/javascripts/store/search.js
+++ app/assets/javascripts/store/search.js
@@ -6,9 +6,11 @@ function initiate_filter() {
 	$('#search-filter li.filter').hover(
 		function() {
 			$(this).find('ul.filter-options').show();
+			$(this).addClass('expand');
 		},
 		function() {
 			$(this).find('ul.filter-options').hide();
+			$(this).removeClass('expand');
 		}
 	);
 }
